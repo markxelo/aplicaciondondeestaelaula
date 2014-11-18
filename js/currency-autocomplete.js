b92bf062f86acaee7b14379d55e244b98f2d7a72
markxelo <mark_xelo@hotmail.com>
2014-11-17 22:58:50 -0300

    se agrega libreria jQueryMobile

--- js/currency-autocomplete.js
+++ js/currency-autocomplete.js
@@ -1,17 +1,17 @@
 $(function(){
 	
 	
-alert('inicio');
+
   var currencies = [    
     { value: ' ', data: '' },
   ];
 
   var puntos;
   
-  $.getJSON("http://dondeestaelaula.esy.es/index.php/api/posts", function(data) {  
+  $.getJSON("http://localhost/dondeestaelaulayii/index.php/api/posts", function(data) {  
 	   $.each(data, function(index, objetoDelListado)
 	   {	
-			alert(objetoDelListado.nombre);
+			//alert(objetoDelListado.nombre);
 			
 			puntos={ value: objetoDelListado.nombre, data: objetoDelListado.id };
 			currencies[currencies.length] = puntos;
@@ -23,7 +23,7 @@ alert('inicio');
   $('#autocomplete').autocomplete({
     lookup: currencies,
     onSelect: function (suggestion) {
-      alert("tal cual");
+      alert("va mejorando");
     }
   });
   
